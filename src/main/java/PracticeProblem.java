df8fa147dde5aa2bc81200426aab4c0e8ba77aa2
usain-boltsMM <343421046@tdsb.ca>
2026-03-26 20:55:09 +0000

    codespace is back

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,50 @@
-Import Java.util.Scanner
+import java.util.Scanner;
 
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        // Test evenOrOdd
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        System.out.println("evenOrOdd: " + evenOrOdd(num));
 
+        // Test teacherOrStudent
+        scanner.nextLine(); // clear buffer
+        System.out.print("Enter a name: ");
+        String name = scanner.nextLine();
+        System.out.println("teacherOrStudent: " + teacherOrStudent(name));
+
+        // Test fartherFromZero
+        System.out.print("Enter another number: ");
+        int num2 = scanner.nextInt();
+        System.out.println("fartherFromZero: " + fartherFromZero(num2));
+    }
+
+    public static String evenOrOdd(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
+    public static String teacherOrStudent(String word) {
+        if (word.equals("Kalisz")) {
+            return "Teacher";
+        } else {
+            return "Student";
+        }
+    }
+
+    public static int fartherFromZero(int num) {
+        if (num > 0) {
+            return num + 5;
+        } else if (num < 0) {
+            return num - 5;
+        } else {
+            return 0;
+        }
+    }
 }
